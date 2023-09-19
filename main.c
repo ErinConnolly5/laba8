d6f99dca45112f3bb61821ce3e1a84e060c499f6
ananas <erinconnolly5b@rambler.ru>
2023-09-19 19:53:26 +0300

    Добавление суммы

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <sdt.h>
 int main()
 {
-	int a, b;
+	int a, b,sum;
 	printf("Введите число а\n");
 	scanf("%d", &a);
 	printf("Введите число b\n");
 	scanf("%d", &b);
+	int sum = a + b;
+	printf("Cумма введенных чисел равна = %d", sum);
 }
